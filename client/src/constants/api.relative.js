84ecf80fa85f6744e69deef0760027c3ebcc7549
Toushif <toushif.haq@gmail.com>
2021-10-02 04:10:17 +0530

    new build client

--- client/src/constants/api.relative.js
+++ client/src/constants/api.relative.js
@@ -1,7 +1,7 @@
 export const Mock_Environment = false;
 export const Json_Server_URL = "/constants/json";
-export const Base_URL = "http://localhost:3001/api";
-// export const Base_URL = "https://greenytale.herokuapp.com/api";
+// export const Base_URL = "http://localhost:3001/api";
+export const Base_URL = "https://greenytale.herokuapp.com/api";
 export const Watson_URL = `https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/e58f4c75-a324-40d1-9fc9-6f5f99b03d81/v1/environments/${process.env.REACT_APP_ENVIRONMENT_ID}/collections/${process.env.REACT_APP_COLLECTION_ID}/query?version=2019-04-30&query=`;
 
 const RelativeAPI = [
