7857a434d972eaa974cac39997679bd8ffae47df
Toushif <toushif.haq@gmail.com>
2022-12-27 17:35:41 +0530

    Cosmetic changes

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ import "./App.scss";
 import "styles/common.scss";
 
 const Login = lazy(() => import("views/login/index"));
-
 const Profile = lazy(() => import("views/profile/index"));
 const Product = lazy(() => import("views/product/index"));
 const WatsonDiscovery = lazy(() => import("views/watson-discovery/index"));
