de337786e46638f6b4277c63f67c3d72d6cfc40e
Toushif <toushif.haq@gmail.com>
2023-04-29 21:07:57 +0530

    Cosmetic changes

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,8 @@ const Main = (props) => {
             .filter((v) => v.ActiveStatus)
             .map((v) => v.RoleID);
         roles = [...new Set([...roles, ...store])];
-    }    
+    }
+    
 
     return (
         <div className="App">
